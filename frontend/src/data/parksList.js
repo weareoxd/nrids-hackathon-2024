1c34721b44c7659b2c683087696745e323b655b7
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2024-09-18 15:37:50 -0700

    HAC-19: Update park card style (#6)

--- frontend/src/data/parksList.js
+++ frontend/src/data/parksList.js
@@ -1,6 +1,8 @@
 export default [
   {
     name: "Golden Ears Park",
+    image:
+      "https://nrs.objectstore.gov.bc.ca/kuwyyf/RS_7390_Golden_Ears_gal_e622f8f345.jpg",
 
     router: {
       name: "golden-ears-park",
@@ -11,6 +13,8 @@ export default [
 
   {
     name: "Cultus Lake Park",
+    image:
+      "https://nrs.objectstore.gov.bc.ca/kuwyyf/cultus_lake_park_41_gal_RS_10712_62d974466e.jpg",
 
     router: {
       name: "cultus-lake-park",
@@ -21,6 +25,8 @@ export default [
 
   {
     name: "Sasquatch Park",
+    image:
+      "https://nrs.objectstore.gov.bc.ca/kuwyyf/RS_7782_Sasquatch_Iain_Robert_Reid_46_gal_e77a88ffd1.jpg",
 
     router: {
       name: "sasquatch-park",
@@ -31,6 +37,8 @@ export default [
 
   {
     name: "Tyhee Lake Park",
+    image:
+      "https://nrs.objectstore.gov.bc.ca/kuwyyf/dbc_131246_dbc_1200_px_Tyhee_9ade1cba78.jpg",
 
     router: {
       name: "tyhee-lake-park",
