80fe8e9468be43443b0cd2e96b9f3584d77b0845
Dylan Rogowsky <dylan@oxd.com>
2024-09-18 14:12:12 -0600

    HAC-00: Refactor, API endpoints

--- backend/app/park/admin.py
+++ backend/app/park/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Facility, FacilityCategory, Feedback, Park, Photo, SubArea
+from .models import Facility, Feedback, Park, Photo
 
 
 @admin.register(Park)
@@ -15,49 +15,29 @@ class ParkAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 
 
-@admin.register(SubArea)
-class SubAreaAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "park",
-    )
-    ordering = ("name",)
-    search_fields = ["name", "park"]
-
-
-@admin.register(FacilityCategory)
-class FacilityCategoryAdmin(admin.ModelAdmin):
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
     list_display = ("name",)
     ordering = ("name",)
     search_fields = ["name"]
 
 
-@admin.register(Facility)
-class FacilityAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "facility_category",
-    )
-    ordering = ("name",)
-    search_fields = ["name", "facility_category"]
-
-
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = (
-        "sub_area",
+        "park",
         "facility",
-        "description",
+        "comments",
         "created_at",
     )
-    ordering = ("sub_area",)
-    search_fields = ["sub_area", "facility", "description"]
+    ordering = ("park", "facility")
+    search_fields = ["park", "facility", "comments"]
 
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = (
         "feedback",
-        "file",
+        "url",
     )
     ordering = ("feedback",)
--- backend/app/park/models.py
+++ backend/app/park/models.py
@@ -14,37 +14,8 @@ class Park(models.Model):
         return self.name
 
 
-class SubArea(models.Model):
-    name = models.TextField()
-    park = models.ForeignKey(
-        Park, related_name="subarea_park", on_delete=models.CASCADE
-    )
-
-    class Meta:
-        ordering = ("name",)
-
-    def __str__(self):
-        return self.name
-
-
-class FacilityCategory(models.Model):
-    name = models.TextField()
-
-    class Meta:
-        ordering = ("name",)
-        verbose_name_plural = "facility categories"
-
-    def __str__(self):
-        return self.name
-
-
 class Facility(models.Model):
     name = models.TextField()
-    facility_category = models.ForeignKey(
-        FacilityCategory,
-        related_name="facility_facilitycategory",
-        on_delete=models.CASCADE,
-    )
 
     class Meta:
         ordering = ("name",)
@@ -55,13 +26,15 @@ class Facility(models.Model):
 
 
 class Feedback(models.Model):
-    sub_area = models.ForeignKey(
-        SubArea, related_name="feedback_subarea", on_delete=models.CASCADE
+    park = models.ForeignKey(
+        Park, related_name="feedback_park", on_delete=models.CASCADE
     )
     facility = models.ForeignKey(
         Facility, related_name="feedback_facility", on_delete=models.CASCADE
     )
-    description = models.TextField()
+
+    facility = models.CharField(max_length=255)
+    comments = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -72,4 +45,13 @@ class Photo(models.Model):
     feedback = models.ForeignKey(
         Feedback, related_name="photo_feedback", on_delete=models.CASCADE
     )
-    file = models.TextField()
+    file_url = models.TextField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True, upload_to="photos/")
+
+    @property
+    def url(self):
+        if self.file_url:
+            return self.file_url
+        if self.file:
+            return self.file.url
+        return None
